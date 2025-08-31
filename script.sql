23b65fa1413e4d8223e5bdf22bbc02e8c2679134
ajitsagale <ajitsagale@gmail.com>
2025-08-31 12:18:32 +0530

    FIX ERROR

--- script.sql
+++ script.sql
@@ -1,53 +1,56 @@
-USE [erpdb]
-GO
 
-/****** Object:  Table [dbo].[users]    Script Date: 7/9/2025 5:54:54 PM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
+-- Drop constraints first (if exist)
+IF OBJECT_ID('dbo.FK_users_classes', 'F') IS NOT NULL
+    ALTER TABLE dbo.users DROP CONSTRAINT FK_users_classes;
+
+IF OBJECT_ID('dbo.FK_users_divisions', 'F') IS NOT NULL
+    ALTER TABLE dbo.users DROP CONSTRAINT FK_users_divisions;
+
+-- Drop the table if it already exists
+IF OBJECT_ID('dbo.users', 'U') IS NOT NULL
+    DROP TABLE dbo.users;
+GO
+
+-- Recreate the table
 CREATE TABLE [dbo].[users](
-	[id] [int] IDENTITY(1,1) NOT NULL,
-	[firstName] [nvarchar](50) NULL,
-	[lastName] [nvarchar](50) NULL,
-	[email] [nvarchar](100) NULL,
-	[phone] [nvarchar](20) NULL,
-	[hashedPassword] [nvarchar](255) NULL,
-	[password] [nvarchar](255) NULL,
-	[role] [int] NULL,
-	[preschoolId] [int] NULL,
-	[childName] [nvarchar](255) NULL,
-	[childAge] [int] NULL,
-	[otp] [varchar](10) NULL,
-	[otpExpiry] [datetime] NULL,
-	[classId] [int] NULL,
-	[divisionId] [int] NULL,
-	[qualification] [varchar](255) NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
-UNIQUE NONCLUSTERED 
-(
-	[email] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+    [id] [int] IDENTITY(1,1) NOT NULL,
+    [firstName] [nvarchar](50) NULL,
+    [lastName] [nvarchar](50) NULL,
+    [email] [nvarchar](100) NULL,
+    [phone] [nvarchar](20) NULL,
+    [hashedPassword] [nvarchar](255) NULL,
+    [password] [nvarchar](255) NULL,
+    [role] [int] NULL,
+    [preschoolId] [int] NULL,
+    [childName] [nvarchar](255) NULL,
+    [childAge] [int] NULL,
+    [otp] [varchar](10) NULL,
+    [otpExpiry] [datetime] NULL,
+    [classId] [int] NULL,
+    [divisionId] [int] NULL,
+    [qualification] [varchar](255) NULL,
+PRIMARY KEY CLUSTERED ([id] ASC),
+UNIQUE NONCLUSTERED ([email] ASC)
+);
 GO
 
-ALTER TABLE [dbo].[users]  WITH CHECK ADD  CONSTRAINT [FK_users_classes] FOREIGN KEY([classId])
-REFERENCES [dbo].[classes] ([id])
+-- Add foreign keys only if parent tables exist
+IF OBJECT_ID('dbo.classes', 'U') IS NOT NULL
+BEGIN
+    ALTER TABLE [dbo].[users]  WITH CHECK ADD CONSTRAINT [FK_users_classes] 
+    FOREIGN KEY([classId]) REFERENCES [dbo].[classes] ([id]);
+    ALTER TABLE [dbo].[users] CHECK CONSTRAINT [FK_users_classes];
+END
+
+IF OBJECT_ID('dbo.divisions', 'U') IS NOT NULL
+BEGIN
+    ALTER TABLE [dbo].[users]  WITH CHECK ADD CONSTRAINT [FK_users_divisions] 
+    FOREIGN KEY([divisionId]) REFERENCES [dbo].[divisions] ([id]);
+    ALTER TABLE [dbo].[users] CHECK CONSTRAINT [FK_users_divisions];
+END
 GO
-
-ALTER TABLE [dbo].[users] CHECK CONSTRAINT [FK_users_classes]
-GO
-
-ALTER TABLE [dbo].[users]  WITH CHECK ADD  CONSTRAINT [FK_users_divisions] FOREIGN KEY([divisionId])
-REFERENCES [dbo].[divisions] ([id])
-GO
-
-ALTER TABLE [dbo].[users] CHECK CONSTRAINT [FK_users_divisions]
-GO
-
-
